e20a98e92712308b854d9ac66c7113482e93e500
h ka <houman.kargaran@gmail.com>
2026-06-08 16:41:47 +1000

    adds embedding activity

---
+++ workflow/activities/vectore_storage_activity.py
@@ -0,0 +1,17 @@
+"""
+activity 1
+1. based on the reference of the cache, return the vectors
+2. set them up based on what Pinecone needs
+3. Push your vectors to the pinecone storage
+4. return the file_id, reference, and pinecone index
+
+activity 2
+1. use the file_id and update the database to set the records to be embedded
+
+activity 3
+1. use the cache reference to delete the redis cache
+
+activity 4
+1. create a query and call the pinecone for the similarity search
+
+"""
