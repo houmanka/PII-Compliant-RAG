0ad6115a399f5e7e3b944c2c36ffa086e6430ad6
h ka <houman.kargaran@gmail.com>
2026-06-08 16:36:32 +1000

    adds embedding activity

--- providers/cache_provider/redis.py
+++ providers/cache_provider/redis.py
@@ -1,3 +1,4 @@
+import pickle
 from typing import Any
 from config import Config
 from providers.cache_provider.contract import CacheProvider
@@ -11,7 +12,8 @@ class RedisStorage(CacheProvider):
         self.redis_connection = redis_connection
 
     def create(self, key: str, value: Any) -> None:
-        self.redis_connection.set(key, value, nx=True)
+        serialized_bytes = pickle.dumps(value)
+        self.redis_connection.set(key, serialized_bytes, nx=True)
 
     def exists(self, key: str) -> bool:
         return bool(self.redis_connection.exists(key))
@@ -25,9 +27,10 @@ class RedisStorage(CacheProvider):
 
     def fetch(self, key: str) -> Any:
         cached_data_bytes = self.redis_connection.get(key)
+        pickled_data = pickle.loads(cached_data_bytes)
         if not cached_data_bytes:
             return None
-        return cached_data_bytes.decode()
+        return pickled_data
 
 
 @register(CacheProviderKind.REDIS)
